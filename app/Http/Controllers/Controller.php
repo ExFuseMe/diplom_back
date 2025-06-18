79110d54e80d750faffe9aa2da52ae3b9b7c3452
enableSshIt <dn007228@gmail.com>
2025-06-18 20:41:57 +0300

    init project

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      title="L5 OpenApi",
+ *      description="Swagger код дипломной работы",
+ *      @OA\Contact(
+ *          email="DRNikitin@stud.kpfu.ru"
+ *      ),
+ * ),
+ * @OA\PathItem(
+ *     path="/api/documentation",
+ * ),
+ */
+
+abstract class Controller
+{
+    //
+}
