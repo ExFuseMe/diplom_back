35a6e28512eea9e043bad67bd121d0d4d0529c19
Данил Никитин <d.nikitin@elyts.ru>
2025-08-21 21:49:48 +0300

    code clean

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -2,17 +2,17 @@
 
 namespace App\Services;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Support\Facades\Auth;
 use Nette\Schema\ValidationException;
 class AuthService
 {
 
-    public function authUser(array $validated)
+    public function authUser(array $validated): ValidationException|Authenticatable
     {
         if (!Auth::attempt($validated)){
             return new ValidationException("Неверный логин или пароль");
         }
-        $user = Auth::user();
-        return $user;
+        return Auth::user();
     }
 }
